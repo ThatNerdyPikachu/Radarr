TorrentW/Rdr

New: NetImport Lists Grouped by Type
Co-Authored-By: ta264 <ta264@users.noreply.github.com>

src/NzbDrone.Core/Datastore/TableMapping.cs
@@ -67,6 +67,7 @@ namespace NzbDrone.Core.Datastore
 
             Mapper.Entity<NetImportDefinition>().RegisterDefinition("NetImport")
                 .Ignore(i => i.Enable)
+                .Ignore(i => i.ListType)
                 .Relationship()
                 .HasOne(n => n.Profile, n => n.ProfileId);
 

ThatNerdyPikachu/Radarr

src/NzbDrone.Core/Datastore/TableMapping.cs
@@ -71,7 +71,8 @@ namespace NzbDrone.Core.Datastore
 
             Mapper.Entity<NetImportDefinition>("NetImport").RegisterModel()
                   .Ignore(x => x.ImplementationName)
-                  .Ignore(i => i.Enable);
+                  .Ignore(i => i.Enable)
+                  .Ignore(i => i.ListType);
 
             Mapper.Entity<NotificationDefinition>("Notifications").RegisterModel()
                   .Ignore(x => x.ImplementationName)